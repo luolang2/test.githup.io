aaa2ea7411c4da50b1f513b36529f2fe5afa8f60
luolang <1609874938@qq.com>
2021-08-01 22:28:59 +0800

    登陆注册完成

--- assects/js/login.js
+++ assects/js/login.js
@@ -10,100 +10,46 @@ $(function () {
         $('.login').show();
         $('.reg').hide();
     })
-    // 表单验证 login
-    $('#username').on('blur', function () {
-        var a = $('#username').val().trim();
-        var mingzi = /^[\u4E00-\u9FA5A-Za-z]+$/;
-        if (a) {
-            if (mingzi.test(a)) {
-                $('.mz').html('名字可用');
-                $('.mz').addClass('green').removeClass('red');
-            } else {
-                $('.mz').html('名字不可用');
-                $('.mz').addClass('red').removeClass('green');
-            }
-        } else {
-            $('.mz').html('名字不能为空');
-            $('.mz').addClass('red').removeClass('green');
-        }
+
+    // 表单验证
+    var form = layui.form;
+    var layer = layui.layer;
+
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ]
     })
 
-    $('#password').on('blur', function () {
-        var a = $('#password').val().trim();
-        var mingzi = /^[a-zA-Z0-9_-]{6,16}$/;
-        if (a) {
-            if (mingzi.test(a)) {
-                $('.mm').html('密码可用');
-                $('.mm').addClass('green').removeClass('red');
-            } else {
-                $('.mm').html('密码不可用');
-                $('.mm').addClass('red').removeClass('green');
-            }
-        } else {
-            $('.mm').html('密码不能为空');
-            $('.mm').addClass('red').removeClass('green');
-        }
-    })
-
-    // 表单验证 register
-    $('#username1').on('blur', function () {
-        var a = $('#username1').val().trim();
-        var mingzi = /^[\u4E00-\u9FA5A-Za-z1-9]+$/;
-        if (a) {
-            if (mingzi.test(a)) {
-                $('.mz1').html('名字可用');
-                $('.mz1').addClass('green').removeClass('red');
-            } else {
-                $('.mz1').html('名字不可用');
-                $('.mz1').addClass('red').removeClass('green');
-            }
-        } else {
-            $('.mz1').html('名字不能为空');
-            $('.mz1').addClass('red').removeClass('green');
-        }
-    })
-    $('#password1').on('blur', function () {
-        var a = $('#password1').val().trim();
-        var mingzi = /^[a-zA-Z0-9_-]{6,16}$/;
-        if (a) {
-            if (mingzi.test(a)) {
-                $('.mm1').html('密码可用');
-                $('.mm1').addClass('green').removeClass('red');
-            } else {
-                $('.mm1').html('密码不可用');
-                $('.mm1').addClass('red').removeClass('green');
-            }
-        } else {
-            $('.mm1').html('密码不能为空');
-            $('.mm1').addClass('red').removeClass('green');
-        }
-    })
-
-    $('#repassword').on('blur', function () {
-        var a = $('#repassword').val().trim();
-        var b = $('#password1').val().trim();
-        if (a) {
-            if (a === b) {
-                $('.re').html('密码相同');
-                $('.re').addClass('green').removeClass('red');
-            } else {
-                $('.re').html('密码不相同');
-                $('.re').addClass('red').removeClass('green');
-            }
-        } else {
-            $('.re').html('此项不能为空');
-            $('.re').addClass('red').removeClass('green');
-        }
-    })
-
-
+    // reg
     $('#form_reg').on('submit', function (e) {
         e.preventDefault();
-        $.post('http://api-breakingnews-web.itheima.net/api/reguser', { 'username': $('#username1').val(), 'possword': $('#password1').val() }, function (res) {
+        $.post('/api/reguser', { username: $('.username2').val(), password: $('.password2').val() }, function (res) {
             if (res.status !== 0) {
-                return console.log(res.message);
+                return layer.msg(res.message);
             }
-            console.log('注册成功');
+            layer.msg('注册成功,请登录');
+            // 模拟文档点击行为
+            $('#link_login').click();
+        })
+    })
+
+    // login
+    $('#form_login').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: '/api/login',
+            method: 'post',
+            // 快速获取表单的数据
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登陆失败');
+                }
+                layer.msg('登陆成功');
+                // 跳转页面到主页
+                location.href = '/index.html';
+            }
         })
     })
 })
